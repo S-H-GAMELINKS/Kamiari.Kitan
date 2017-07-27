68da264534366ec27e7cdac0d3dc4a7f5c4cfadd
S.H <gamelinks007@gmail.com>
2017-07-27 22:44:30 +0900

    ゲームメニュー呼び出し時のBGM停止処理の実装

--- SYSTEM/script.rb
+++ SYSTEM/script.rb
@@ -89,6 +89,15 @@ class Script
 							#エンターキーで次の文字列を呼び出す
 							if Input.mouse_push?(M_LBUTTON); break; end
 
+							#ゲームメニュー呼び出し時のBGM停止処理
+							if Input.mouse_push?(M_RBUTTON) then
+								if @bgm == nil then
+								
+								else
+									@bgm.stop
+								end
+							end
+
 							#ゲームメニュー呼び出し
 							gamemenu_call(messagebox, font, mouse, lineno)
 
